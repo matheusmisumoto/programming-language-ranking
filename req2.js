b8dc516d56f7bdc66953497ac1d7320a09b57caa
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2019-11-25 16:16:12 -0300

    V.0.2.2 - Pequenos ajustes
    Pequenos ajustes

--- req2.js
+++ req2.js
@@ -2,77 +2,126 @@ $(criarEvento);
 
 
 function criarEvento(){
-	$('button').click(function(){
-		var languages = ['JavaScript'];
+		var languages = ['javascript', 'java', 'php', 'haskell', 'ruby'];
+		table = [];
 		for(let s in languages){
-			var listaLinguagens = document.createElement('ul');
-			var itemLinguagens = document.createElement('li');
-			itemLinguagens.innerHTML = languages;
+			table[s] = [ languages[s] ];
 
-			listaResultado = document.createElement('ul');
-			sendReq('https://www.google.com/search?q='+languages[s]+'&sourceid=chrome&ie=UTF-8');
-			sendReq('https://www.stackoverflow.com/questions/tagged/'+languages[s]);
-			sendReq('https://www.github.com/search?q=language%3A'+languages[s]);
+			//var itemLinguagens = document.createElement('li');
+			//itemLinguagens.innerHTML = languages;
 
-			listaLinguagens.appendChild(itemLinguagens);
-			listaLinguagens.appendChild(listaResultado);
-			document.body.appendChild(listaLinguagens);
+			//listaResultado = document.createElement('ul');
+			sendReq('https://google.com/search?q='+languages[s]+'&sourceid=chrome&ie=UTF-8',s);
+			sendReq('https://stackoverflow.com/questions/tagged/'+languages[s],s);
+			setTimeout(function() { sendReq('https://github.com/search?q=language%3A'+languages[s],s) }, 2500);
+
+			//listaLinguagens.appendChild(itemLinguagens);
+			//listaLinguagens.appendChild(listaResultado);
+			//document.body.appendChild(listaLinguagens);
 		}
-	});
+
+		setTimeout(function() { montaTabela(table); }, 5000);
+ 	    // retorna um array onde:
+		// table = [ [ linguagem, google, stackoverflow, github ], ... ]
 }
 
+function montaTabela(ordenados){
+	var lista = document.querySelector('#lista');
+	ordenados.sort(function (a, b) {
+		if (a[3] > b[3]) {
+		  return -1;
+		}
+		if (a[3] < b[3]) {
+		  return 1;
+		}
+		// a must be equal to b
+		return 0;
+	  });
+	
+	for (let i = 0; i < ordenados.length; i++){
+		var item = document.createElement('li');
+		item.className = 'border';
+	
+		var boxConteudo = document.createElement('div');
+		boxConteudo.className = 'content';
+	
+		var colocacao = document.createElement('span');
+		var nomeLinguagem = document.createElement('h2');
+		var listaNumeros = document.createElement('ul');
+	
+		var google = document.createElement('li');
+		var stackoverflow = document.createElement('li');
+		var github = document.createElement('li'); 
+	
+		colocacao.innerHTML = i+1;
+		nomeLinguagem.innerHTML = ordenados[i][0];
+		google.innerHTML = ordenados[i][1];
+		stackoverflow.innerHTML = ordenados[i][2];
+		github.innerHTML = ordenados[i][3];
 
-function rasparPagina(documento, url){
+		listaNumeros.appendChild(github);
+		listaNumeros.appendChild(stackoverflow);
+		listaNumeros.appendChild(google);
+		
+		boxConteudo.appendChild(colocacao);
+		boxConteudo.appendChild(nomeLinguagem);
+		boxConteudo.appendChild(listaNumeros);
+		
+		item.appendChild(boxConteudo);
+		lista.appendChild(item);
+	}
+	
+	// atualiza horário
+	document.querySelector('header > span').innerHTML = 'Gerado em ' + new Date().toLocaleString('pt-BR');
+}
+
+function rasparPagina(documento, url, s){
 
 	// Como cada site precisa de uma regra específica, é necessário 
 	// determinar de qual site estamos raspando os dados
-	if(url.split('.')[1] == 'google'){
-		let linhaContador = documento.querySelectorAll('#resultStats');
-		linhaContador.forEach(function(inf){
+	if(url.split('/')[2] == 'google.com'){
+		let linhaContador = documento.querySelector('#resultStats');
 			// Deixa só os números do resultado
-			let filtro = inf.innerHTML.replace('Aproximadamente ', '');
-			filtro = filtro.split('resultados')[0];
+			let filtro = linhaContador.innerHTML.replace('Aproximadamente ', '');
+			filtro = filtro.replace(/[ ,.]/g, "");;
+			table[s][1] = parseInt(filtro.split('resultados')[0]);
 
-			let itemResultado = document.createElement('li');
-			itemResultado.innerHTML = 'Google: '+filtro;
-
-			listaResultado.appendChild(itemResultado);
-		});
+			//let itemResultado = document.createElement('li');
+			//itemResultado.innerHTML = 'Google: '+filtro;
+			//listaResultado.appendChild(itemResultado);
 	}
-	else if(url.split('.')[1] == 'stackoverflow'){
-		let linhaContador = documento.querySelectorAll('.fs-body3');
-		linhaContador.forEach(function(inf){
+	else if(url.split('/')[2] == 'stackoverflow.com'){
+		let linhaContador = documento.querySelector('.fs-body3');
 			// Deixa só os números do resultado
-			let filtro = inf.innerText.replace('questions', '');
+			let filtro = linhaContador.innerText.replace(/\s/g, '');
+			table[s][2] = parseInt(filtro.replace(/[ ,.]/g, ""));
 
-			let itemResultado = document.createElement('li');
-			itemResultado.innerHTML = 'StackOverflow: '+filtro;
-
-			listaResultado.appendChild(itemResultado);
-		});
+			//let itemResultado = document.createElement('li');
+			//itemResultado.innerHTML = 'StackOverflow: '+filtro;
+			//listaResultado.appendChild(itemResultado);
 	}
-	else if(url.split('.')[1] == 'github'){
-		let linhaContador = documento.querySelectorAll('h3 > span');
-		linhaContador.forEach(function(inf){
+	else if(url.split('/')[2] == 'github.com'){
+		let linhaContador = documento.querySelector('.pb-3 > h3 > span.v-align-middle');
 			// Deixa só os números do resultado
-			let filtro = inf.innerHTML.split('a')[0];
-			filtro = filtro.replace('Showing', '');
+			let filtro = linhaContador.innerHTML.split('a')[0];
+			filtro = filtro.replace(/(\r\n|\n|\r)/gm,"")
+			filtro = filtro.replace(/[ ,.]/g, "");
+			table[s][3] = parseInt(filtro.replace("Showing", ""));
+			table[s][4] = table[s][1] + table[s][2] + table[s][3];
 					
-			let itemResultado = document.createElement('li');
-			itemResultado.innerHTML = 'GitHub: '+filtro;
-
-			listaResultado.appendChild(itemResultado);
-		});
+			//let itemResultado = document.createElement('li');
+			//itemResultado.innerHTML = 'GitHub: '+filtro;
+			//listaResultado.appendChild(itemResultado);
 	}
 }
 
-function sendReq(url){	
+function sendReq(url,s){	
 	var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
 					var parser = new DOMParser();
 					var documento = parser.parseFromString(this.responseText, 'text/html');
-					rasparPagina(documento, url);
+					rasparPagina(documento, url, s);
 		}
 	};
 	//https://cors-anywhere.herokuapp.com/
